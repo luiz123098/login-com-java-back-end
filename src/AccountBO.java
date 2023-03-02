44ea006d947d86b6377cf25733a3f58ff77659d4
luiz neto <luiznetopaluti@hotmail.com>
2023-03-02 20:26:43 -0300

    ojbn

---
+++ src/AccountBO.java
@@ -0,0 +1,11 @@
+public class AccountBO {
+
+    public boolean AccountFilter(String user, String password) {
+        AccountMB accountMB = new AccountMB();
+        if(accountMB.accountRegister();)
+        accountMB.accountRegister();
+        return null
+    }
+
+
+}
